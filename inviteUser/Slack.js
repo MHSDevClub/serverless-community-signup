f4ceb2fbd81351cf3d502e8b3700b667726dbee2
Brian Lee <briancmlee23@gmail.com>
2018-01-19 23:02:54 +1100

    Add response handling to Slack.js

--- inviteUser/Slack.js
+++ inviteUser/Slack.js
@@ -1,18 +1,25 @@
 class Slack {
-    constructor(apiToken, teamName) {
+    constructor(apiToken, teamName, fetch) {
         this.apiToken = apiToken;
         this.teamName = teamName;
+        this.fetch = fetch;
     }
 
-    invite(email, firstName, lastName) {
+    invite(email, firstName, lastName, callback) {
         const requestUrl = `https://${this.teamName}.slack.com/api/users.admin.invite`;
 
-        fetch(requestUrl, {
+        this.fetch(requestUrl, {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: `token=${this.apiToken}&email=${email}&first_name=${firstName}&last_name=${lastName}`
         })
-            .then(res => callback(res))
+            .then(res => callback(null, {
+                statusCode: res.statusCode,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    res
+                })
+            }))
             .catch(err => callback(res));
     }
 }
